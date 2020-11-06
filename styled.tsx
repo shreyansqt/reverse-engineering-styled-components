406e7f4af4b695f9cee8a4312270ab9b0c169315
Shreyans Jain <shreyans@makersden.io>
2020-11-06 20:53:59 +0530

    add parcel to package.json, improved comments

--- styled.tsx
+++ styled.tsx
@@ -1,15 +1,24 @@
 import * as React from "react";
 
+// create & insert our style tag
 const styleTag = document.createElement("style");
 document.head.appendChild(styleTag);
 
+// hashing function, generates a numerical string
+// https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+const hashCode = (s) =>
+  s.split("").reduce(function (a, b) {
+    a = (a << 5) - a + b.charCodeAt(0);
+    return a & a;
+  }, 0);
+
 const styled = (Tag) => (styleArr) => {
-  // use style string to randomly generate a className
   const styles = styleArr[0].trim();
+  // use styles string to randomly generate a className
   const className = `style${hashCode(styles)}`;
   const styleStr = `.${className} {${styles}}`;
 
-  // check if styles exist already & prevent duplicates
+  // check if styles already exist & prevent duplicate
   const isDuplicate = styleTag.innerText.indexOf(styleStr) > -1;
   if (!isDuplicate) {
     styleTag.innerText = `${styleTag.innerText} .${className} {${styles}}`;
@@ -31,9 +40,3 @@ tagNames.forEach((tagName) => {
 });
 
 export default styled;
-
-const hashCode = (s) =>
-  s.split("").reduce(function (a, b) {
-    a = (a << 5) - a + b.charCodeAt(0);
-    return a & a;
-  }, 0);
